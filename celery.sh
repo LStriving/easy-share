36948ff2d8f14e7550d5ef88db5035ec140417e3
Lstriving <2625847986@qq.com>
2023-07-13 19:59:03 +0800

    start or stop with a more elegant way

--- celery.sh
+++ celery.sh
@@ -1,2 +1,2 @@
-pkill -f celery
-nohup celery -A EasyShare worker > log/celery.log &
+celery multi stop 1 -A EasyShare --pidfile=/home/lyr/project/easy-share/log/%n.pid --logfile=log/celery.log || true
+celery multi start 1 -A EasyShare --logfile=log/celery.log -l info --pidfile=/home/lyr/project/easy-share/log/%n.pid
