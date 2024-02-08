44a69bf6b4fee4481b7b20c831fd7548eb68c1b6
LStriving <2625847986@qq.com>
2024-02-08 12:49:51 +0800

    fix redis connection error when using Docker

--- EasyShare/settings/prod.py
+++ EasyShare/settings/prod.py
@@ -13,15 +13,15 @@ SECURE_SSL_REDIRECT = False
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://redis:6379",
         "TIMEOUT": 60 * 60 * 24 * 7,
     }
 }
 
 # celery configuration
 app.conf.update(
-    broker_url='redis://127.0.0.1:6379/8',
-    result_backend='redis://127.0.0.1:6379/9',
+    broker_url='redis://redis:6379/8',
+    result_backend='redis://redis:6379/9',
     timezone='Asia/Shanghai',
     worker_cancel_long_running_tasks_on_connection_loss=True,
     task_acks_late=True,
--- EasyShare/settings/test.py
+++ EasyShare/settings/test.py
@@ -2,20 +2,20 @@ from .base import *
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EasyShare.settings.test')
 
-ALLOWED_HOSTS = ['127.0.0.1','hailin545.cn']
+ALLOWED_HOSTS = ['127.0.0.1','hailin545.cn','luohailin.cn']
 
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://redis:6379",
         "TIMEOUT": 60 * 60 * 3,
     }
 }
 
 # celery configuration
 app.conf.update(
-    broker_url='redis://127.0.0.1:6379/8',
-    result_backend='redis://127.0.0.1:6379/9',
+    broker_url='redis://redis:6379/8',
+    result_backend='redis://redis:6379/9',
     timezone='Asia/Shanghai',
     worker_cancel_long_running_tasks_on_connection_loss=True,
     task_acks_late=True,
