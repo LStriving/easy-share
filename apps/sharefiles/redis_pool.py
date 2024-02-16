d684a032c380dd1208894896782842aa9a329abc
LStriving <2625847986@qq.com>
2024-02-17 03:51:17 +0800

    fix redis host name bug

--- apps/sharefiles/redis_pool.py
+++ apps/sharefiles/redis_pool.py
@@ -1,3 +1,8 @@
 import redis
-
-POOL=redis.ConnectionPool(host='127.0.0.1',port=6379,max_connections=100)
+import os
+env = os.environ.get("DJANGO_SETTINGS_MODULE").split('.')[-1]
+if env == 'dev':
+    host = '127.0.0.1'
+else:
+    host = 'redis'
+POOL=redis.ConnectionPool(host=host,port=6379,max_connections=100)
