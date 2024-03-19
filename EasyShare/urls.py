fd869cec291f130f5e656e36b4ea6ba4b3f85f6e
LStriving <2625847986@qq.com>
2024-03-19 22:24:07 +0800

    🌐homepage demo2; 🔗optimize logic for redirecting

--- EasyShare/urls.py
+++ EasyShare/urls.py
@@ -34,5 +34,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url('media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
     url(r'^doc$', schema_view,name='documentation'),
-    path("",auth_views.LoginView.as_view(template_name='./login.html'),name='login')
+    path("",include("surgery.urls")),
+    # path("",auth_views.LoginView.as_view(template_name='./login.html'),name='login')
 ]
